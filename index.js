3d09ef7983c4e6db7d3a4fe8318849381f654421
David <dwd7290@yahoo.com>
2018-03-19 23:30:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  var serving = katzDeliLine[0];
+  
+  if (serving === '' || serving === null || serving === undefined) {
+    return "There is nobody waiting to be served!";
+  } else {
+    katzDeliLine.shift();
+    return `Currently serving ${serving}.`;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var line = [];
+  if (katzDeliLine.length !== 0) {
+    for(var i = 0; i < katzDeliLine.length; i++) {
+      if (i === 0) {
+        line.pusj(`The line is currently: ${i+1}. `);
+      }
+    }
+  }
+}
